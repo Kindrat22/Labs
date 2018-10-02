97663099bbee66f7a0f1d7e1a1fd2b60b581bf6b
Kindrat22 <43355118+Kindrat22@users.noreply.github.com>
2018-10-02 11:26:20 +0300

    Update Lab2.c

--- Lab2.c
+++ Lab2.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <math.h>
 
+int main(){
+    double a, n, sum, e;
+    e = 0.0001;
+    a = 1;
+    n = 0;
+    sum = 1;
+    while( a > e){
+        a = (1/ (4*n + 20)) *a;
+        sum = sum + a;
+        n++;
+    }
+    printf("%lf\n", sum);
+}
