5b0c3ef95e8754646d2c3389489844e0fc934709
Kindrat22 <43355118+Kindrat22@users.noreply.github.com>
2018-10-09 10:57:30 +0300

    Update Lab3.c

--- Lab3.c
+++ Lab3.c
@@ -3,17 +3,27 @@
 
 int main(void)
 {
-  double  logar, l, tng, y;
+  double  logar, l, tng, y, s, sn;
   double x = 0.1;
-  double sum = 0;
+  double sumY = 0;
+  double nn = 1;
 for(int a = 1; a <= 8; a++)
     {
       l = (1 + x) / (1 - x);
       tng = (atan(x)/2);
       logar = log(l);
       y = logar/4 + tng;
-      sum = sum + y;
-      x = x + 0.1;
-      printf("x%d  =  %f \n", a, sum );
+      sumY = sumY + y;
+      s = x + pow(x, 5)/5;
+      sn = nn + pow(nn, 5)/5;
+        for(double n = 0; n<3; n++){
+        s = s + pow(x, 4*n + 1)/(4*n + 1);
+          }
+        for(double n = 0; n<3; n++){
+          sn = sn + pow(nn, 4*n + 1)/(4*n + 1);
+          }
+      printf("x = %f,   SN =%f    SE =%f   y = %f \n", x, sn, s, sumY );
+      x += 0.1;
+      nn++;
     }
 }
