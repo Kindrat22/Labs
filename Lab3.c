ce2587925b7e249c5094f054d1c45eaec4d6fc94
Kindrat22 <43355118+Kindrat22@users.noreply.github.com>
2018-10-09 11:53:30 +0300

    Update Lab3.c

--- Lab3.c
+++ Lab3.c
@@ -3,27 +3,26 @@
 
 int main(void)
 {
-  double  logar, l, tng, y, s, sn;
-  double x = 0.1;
-  double sumY = 0;
-  double nn = 1;
-for(int a = 1; a <= 8; a++)
+  double  logar, l, tng, y, a;
+  double x;
+  double se = 0;
+  double s = 0;
+  double n;
+for(x = 0.1; x <= 0.8; x +=0.07)
     {
       l = (1 + x) / (1 - x);
       tng = (atan(x)/2);
       logar = log(l);
       y = logar/4 + tng;
-      sumY = sumY + y;
-      s = x + pow(x, 5)/5;
-      sn = nn + pow(nn, 5)/5;
-        for(double n = 0; n<3; n++){
+
+      for(n = 0; n<4; n++){
         s = s + pow(x, 4*n + 1)/(4*n + 1);
-          }
-        for(double n = 0; n<3; n++){
-          sn = sn + pow(nn, 4*n + 1)/(4*n + 1);
-          }
-      printf("x = %f,   SN =%f    SE =%f   y = %f \n", x, sn, s, sumY );
-      x += 0.1;
-      nn++;
+        }
+      while(a< 0.0001){ //Доробити цикл 
+        a = pow(x, 4*n + 1)/(4*n + 1);
+          se = se + pow(x, 4*n + 1)/(4*n + 1);
+        }
+
+      printf("x = %f,   SN =%f   SE =%f   y = %f \n", x, s, se, y );
     }
 }
